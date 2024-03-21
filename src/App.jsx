7450063ea4db5a6079710b9958eee6638922d05d
mminutes <96639427+mminutes@users.noreply.github.com>
2024-03-20 21:57:23 -0400

    Created Post component

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Post from './components/Post';
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Fakebook!</h1>
+      {/* CreatePostForm */}
+      {/* Feed */}
+
+      <Post />
+      
+    </div>
+      )
+      }
+
+export default App
