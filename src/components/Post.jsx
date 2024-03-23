1eff91995294d9f0659b853432b6de361b4c62c6
mminutes <96639427+mminutes@users.noreply.github.com>
2024-03-22 22:06:46 -0400

    Passed props to Comment component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Comment from './Comment';
 
 function Post(){
@@ -9,7 +10,9 @@ function Post(){
             <p>This is a test post we'll edit later!</p>
             <p>Likes: 30</p>
             <p>Comments:</p>
-            <Comment />
+            <Comment content="This is a test comment!" />
+            <Comment content="This is another test comment!" />
+            <Comment content="This is yet another test comment!" />
         </div>
     )
 }
